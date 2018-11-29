d0ebad291840b77c172a89bd628dfcb6ca560802
Tiffany Kim <minikim9@hotmail.com>
2018-11-28 17:04:37 -0800

    Completed fake sign in sign out functionality

--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -1,12 +1,17 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE = {
-
+    auth: false,
 };
 
 // here we create the reducer itself 
 export default (state=DEFAULT_STATE, action)=>{
     switch(action.type){
+        case types.SIGN_IN:
+        case types.SIGN_UP:
+            return {auth: true}
+        case types.SIGN_OUT:
+            return {auth: false};
         default:
             return state;
     }
